6d1c28787fa55d0bab80dfbaeb0671b6a3873dfe
Ruffin Arnaud <arnaud.ruffin@orange.com>
2020-12-12 21:41:54 +0100

    computation OK

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,169 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
     <v-main>
-      <HelloWorld/>
+      <v-container>
+        <v-card elevation="2" shaped class="mx-auto my-12" max-width="374">
+          <v-card-title>Dose équivalente biologique: </v-card-title>
+          <p class="text-center font-weight-black">{{ result }}</p>
+
+          <v-card-text>
+            <v-form ref="form">
+              <v-select
+                type="number"
+                v-model="selectedOrgan"
+                :hint="
+                  selectedOrgan
+                    ? `${selectedOrgan.name},⍺β: ${selectedOrgan.value}`
+                    : 'organe'
+                "
+                :items="organs"
+                item-text="name"
+                item-value="name"
+                label="Sélectionnez l'organe à risque"
+                persistent-hint
+                return-object
+                single-line
+              ></v-select>
+
+              <v-text-field
+                v-model="dt"
+                step="any"
+                type="number"
+                label="Dose totale"
+              ></v-text-field>
+              <v-text-field
+                type="number"
+                v-model="dfi"
+                label="Dose par fraction initiale"
+              ></v-text-field>
+              <v-text-field
+                type="number"
+                step="any"
+                v-model="dfs"
+                label="Dose par fraction souhaitées"
+              ></v-text-field>
+            </v-form>
+          </v-card-text>
+        </v-card>
+
+        <v-banner>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
+          cursus nec sem id malesuada. Curabitur lacinia sem et turpis euismod,
+          eget elementum ex pretium.
+          <template v-slot:actions>
+            <v-btn text color="primary"> Dismiss </v-btn>
+            <v-btn text color="primary"> Retry </v-btn>
+          </template>
+        </v-banner>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import Vue from "vue";
+import Component from "vue-class-component";
+import Organ, { ALL_ORGANS } from "./bean/Organ";
 
-export default Vue.extend({
-  name: 'App',
+@Component
+export default class App extends Vue {
+  private organs: Organ[] = ALL_ORGANS;
+  selectedOrgan: Organ | null = null;
 
-  components: {
-    HelloWorld,
-  },
+  dt: number | null = null;
+  dfi: number | null = null;
+  dfs: number | null = null;
 
-  data: () => ({
-    //
-  }),
-});
+  showAlert(message: string) {
+    console.log(message);
+  }
+  get result(): number | null {
+    if (this.selectedOrgan && this.dfs && this.dfi && this.dt) {
+      //TODO animate on new rendering
+
+      const organevalue = this.selectedOrgan!.value;
+      const dosefraction1 = this.dfi!;
+      const dosefraction2 = this.dfs!;
+      const dosetotale1 = this.dt!;
+
+      console.log(organevalue);
+      console.log(dosefraction1);
+      console.log(dosefraction2);
+      console.log(dosetotale1);
+
+      const doseeq =
+        (dosetotale1 * (organevalue + +dosefraction1)) /
+        (organevalue + +dosefraction2);
+
+      console.log(dosetotale1 * (organevalue + +dosefraction1));
+      console.log(organevalue + +dosefraction2);
+      if (dosefraction1 > 8) {
+        this.showAlert(
+          "Vous sortez des limites du modèle pour la dose initiale, les doses calculées par le modèle ne peuvent être considérées comme valides."
+        );
+      }
+      if (dosefraction1 < 1) {
+        this.showAlert(
+          "Vous sortez des limites du modèle pour la dose initiale, les doses calculées par le modèle ne peuvent être considérées comme valides."
+        );
+      }
+      if (dosefraction2 > 8) {
+        this.showAlert(
+          "Vous sortez des limites du modèle pour la dose désirée, les doses calculées par le modèle ne peuvent être considérées comme valides."
+        );
+      }
+      if (dosefraction2 < 1) {
+        this.showAlert(
+          "Vous sortez des limites du modèle pour la dose désirée, les doses calculées par le modèle ne peuvent être considérées comme valides."
+        );
+      }
+
+      // verifier contraintes
+      if (organevalue == 1 && doseeq > 26) {
+        this.showAlert(
+          "Le guide des procédures de radiothérapie externe recommande de ne pas dépasser 26 Gy en dose totale pour le foie."
+        );
+      }
+      if (organevalue == 1 && doseeq > 35) {
+        this.showAlert(
+          "Le guide des procédures de radiothérapie externe recommande de ne pas dépasser 35 Gy en dose moyenne pour chaque oeil."
+        );
+      }
+      if (organevalue == 2 && doseeq > 45) {
+        this.showAlert(
+          "Le guide des procédures de radiothérapie externe recommande de ne pas dépasser 45 Gy en dose totale pour la moelle."
+        );
+      }
+      if (organevalue == 2 && doseeq > 30) {
+        this.showAlert(
+          "Le guide des procédures de radiothérapie externe recommande de ne pas dépasser 30 Gy en dose moyenne pour la parotide."
+        );
+      }
+      if (organevalue == 2 && doseeq > 50) {
+        this.showAlert(
+          "Le guide des procédures de radiothérapie externe recommande de ne pas dépasser 50 Gy en dose totale pour le grêle."
+        );
+      }
+      if (organevalue == 3 && doseeq > 35) {
+        this.showAlert(
+          "Le guide des procédures de radiothérapie externe recommande de ne pas dépasser 35 Gy en dose totale pour le coeur."
+        );
+      }
+      if (organevalue == 3 && doseeq > 20) {
+        this.showAlert(
+          "Le guide des procédures de radiothérapie externe recommande de ne pas dépasser 20 Gy en dose totale pour chaque rein."
+        );
+      }
+      if (organevalue == 3 && doseeq > 50) {
+        this.showAlert(
+          "Le guide des procédures de radiothérapie externe recommande de ne pas dépasser 50 Gy en dose totale pour le tronc cérébral."
+        );
+      }
+      console.log(doseeq);
+      return Math.round(doseeq * 100) / 100;
+    } else {
+      return null;
+    }
+  }
+}
 </script>

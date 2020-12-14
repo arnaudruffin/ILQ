7e52c9d3d8ef60cc18017dd541c6288a6adf9fc3
Ruffin Arnaud <arnaud.ruffin@orange.com>
2020-12-14 20:50:43 +0100

    rror handling

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,17 @@
   <v-app>
     <v-main>
       <v-container>
-        <v-alert dense outlined type="error">
-          I'm a dense alert with the <strong>outlined</strong> prop and a
-          <strong>type</strong> of error
+
+        <v-alert dense outlined type="error" v-if="diError">
+          {{ diError }}
+        </v-alert>
+
+        <v-alert dense outlined type="error" v-if="ddError">
+          {{ ddError }}
+        </v-alert>
+
+        <v-alert dense outlined type="error" v-if="organError">
+          {{ organError }}
         </v-alert>
 
         <v-card elevation="2">
@@ -13,53 +21,53 @@
           <v-card-text>
             <v-form ref="form">
               <v-select
-                type="number"
-                v-model="selectedOrgan"
-                :hint="
+                  type="number"
+                  v-model="selectedOrgan"
+                  :hint="
                   selectedOrgan
                     ? `${selectedOrgan.name},⍺β: ${selectedOrgan.value}`
                     : 'organe'
                 "
-                :items="organs"
-                item-text="name"
-                item-value="name"
-                label="Sélectionnez l'organe à risque"
-                persistent-hint
-                return-object
-                single-line
+                  :items="organs"
+                  item-text="name"
+                  item-value="name"
+                  label="Sélectionnez l'organe à risque"
+                  persistent-hint
+                  return-object
+                  single-line
               ></v-select>
 
               <v-text-field
-                v-model="dt"
-                step="any"
-                type="number"
-                label="Dose totale"
+                  v-model="dt"
+                  step="any"
+                  type="number"
+                  label="Dose totale"
               ></v-text-field>
               <v-text-field
-                type="number"
-                min="1"
-                max="6"
-                v-model="dfi"
-                label="Dose par fraction initiale"
+                  type="number"
+                  min="1"
+                  max="6"
+                  v-model="dfi"
+                  label="Dose par fraction initiale"
               ></v-text-field>
               <v-text-field
-                type="number"
-                step="any"
-                v-model="dfs"
-                min="1"
-                max="6"
-                label="Dose par fraction souhaitées"
+                  type="number"
+                  step="any"
+                  v-model="dfs"
+                  min="1"
+                  max="6"
+                  label="Dose par fraction souhaitées"
               ></v-text-field>
             </v-form>
 
             Dose équivalente biologique:
             <p class="text-center font-weight-black">{{ result }}
-                       <v-chip v-if="result" class="ma-2 text-center font-weight-black" color="green" text-color="white" x-large>
-              {{ result }}
-            </v-chip>
+              <v-chip v-if="result" class="ma-2 text-center font-weight-black" color="green" text-color="white" x-large>
+                {{ result }}
+              </v-chip>
             </p>
 
-   
+
           </v-card-text>
         </v-card>
 
@@ -73,8 +81,8 @@
             <li>Étalement constant</li>
           </ul>
           <template v-slot:actions>
-            <v-btn text color="primary"> Dismiss </v-btn>
-            <v-btn text color="primary"> Retry </v-btn>
+            <v-btn text color="primary"> Dismiss</v-btn>
+            <v-btn text color="primary"> Retry</v-btn>
           </template>
         </v-banner>
       </v-container>
@@ -85,7 +93,7 @@
 <script lang="ts">
 import Vue from "vue";
 import Component from "vue-class-component";
-import Organ, { ALL_ORGANS } from "./bean/Organ";
+import Organ, {ALL_ORGANS} from "./bean/Organ";
 
 @Component
 export default class App extends Vue {
@@ -96,98 +104,56 @@ export default class App extends Vue {
   dfi: number | null = null;
   dfs: number | null = null;
 
+  organError: string | null = null;
+  ddError: string | null = null;
+  diError: string | null = null;
+
   showAlert(message: string) {
     console.log(message);
   }
+
   get result(): number | null {
+
+
+    if (this.dfi && (this.dfi > 8 || this.dfi < 1)) {
+      this.ddError = "Vous sortez des limites du modèle pour la <strong>dose initiale</strong>, les doses calculées par le modèle ne peuvent être considérées comme valides."
+    } else {
+      this.ddError = null
+    }
+
+    if (this.dfs && (this.dfs > 8 || this.dfs < 1)) {
+      this.diError = "Vous sortez des limites du modèle pour la <strong>dose désirée</strong>, les doses calculées par le modèle ne peuvent être considérées comme valides."
+    } else {
+      this.diError = null
+    }
+
     if (this.selectedOrgan && this.dfs && this.dfi && this.dt) {
       //TODO animate on new rendering
       //TODO afficher les GY
       //TODO show warn as overlay
       //TODO handler errot messag
-      const organevalue = this.selectedOrgan!.value;
-      const dosefraction1 = this.dfi!;
-      const dosefraction2 = this.dfs!;
-      const dosetotale1 = this.dt!;
+      const organValue = this.selectedOrgan.value;
+      console.log(organValue);
+      console.log(this.dfi);
+      console.log(this.dfs);
+      console.log(this.dt);
 
-      console.log(organevalue);
-      console.log(dosefraction1);
-      console.log(dosefraction2);
-      console.log(dosetotale1);
+      const doseEquivalente = (this.dt * (organValue + +this.dfi)) / (organValue + +this.dfs);
 
-      const doseeq =
-        (dosetotale1 * (organevalue + +dosefraction1)) /
-        (organevalue + +dosefraction2);
 
-      console.log(dosetotale1 * (organevalue + +dosefraction1));
-      console.log(organevalue + +dosefraction2);
-      if (dosefraction1 > 8) {
-        this.showAlert(
-          "Vous sortez des limites du modèle pour la dose initiale, les doses calculées par le modèle ne peuvent être considérées comme valides."
-        );
-      }
-      if (dosefraction1 < 1) {
-        this.showAlert(
-          "Vous sortez des limites du modèle pour la dose initiale, les doses calculées par le modèle ne peuvent être considérées comme valides."
-        );
-      }
-      if (dosefraction2 > 8) {
-        this.showAlert(
-          "Vous sortez des limites du modèle pour la dose désirée, les doses calculées par le modèle ne peuvent être considérées comme valides."
-        );
-      }
-      if (dosefraction2 < 1) {
-        this.showAlert(
-          "Vous sortez des limites du modèle pour la dose désirée, les doses calculées par le modèle ne peuvent être considérées comme valides."
-        );
+      // verifier contraintes organe
+      if (this.selectedOrgan.doseMax && doseEquivalente > this.selectedOrgan.doseMax) {
+        this.organError = "Le guide des procédures de radiothérapie externe recommande de ne pas dépasser " + this.selectedOrgan.doseMax + " Gy en dose totale pour cet organe. (" + this.selectedOrgan.name + ")";
+      } else {
+        this.organError = null
       }
 
-      // verifier contraintes
-      if (organevalue == 1 && doseeq > 26) {
-        this.showAlert(
-          "Le guide des procédures de radiothérapie externe recommande de ne pas dépasser 26 Gy en dose totale pour le foie."
-        );
-      }
-      if (organevalue == 1 && doseeq > 35) {
-        this.showAlert(
-          "Le guide des procédures de radiothérapie externe recommande de ne pas dépasser 35 Gy en dose moyenne pour chaque oeil."
-        );
-      }
-      if (organevalue == 2 && doseeq > 45) {
-        this.showAlert(
-          "Le guide des procédures de radiothérapie externe recommande de ne pas dépasser 45 Gy en dose totale pour la moelle."
-        );
-      }
-      if (organevalue == 2 && doseeq > 30) {
-        this.showAlert(
-          "Le guide des procédures de radiothérapie externe recommande de ne pas dépasser 30 Gy en dose moyenne pour la parotide."
-        );
-      }
-      if (organevalue == 2 && doseeq > 50) {
-        this.showAlert(
-          "Le guide des procédures de radiothérapie externe recommande de ne pas dépasser 50 Gy en dose totale pour le grêle."
-        );
-      }
-      if (organevalue == 3 && doseeq > 35) {
-        this.showAlert(
-          "Le guide des procédures de radiothérapie externe recommande de ne pas dépasser 35 Gy en dose totale pour le coeur."
-        );
-      }
-      if (organevalue == 3 && doseeq > 20) {
-        this.showAlert(
-          "Le guide des procédures de radiothérapie externe recommande de ne pas dépasser 20 Gy en dose totale pour chaque rein."
-        );
-      }
-      if (organevalue == 3 && doseeq > 50) {
-        this.showAlert(
-          "Le guide des procédures de radiothérapie externe recommande de ne pas dépasser 50 Gy en dose totale pour le tronc cérébral."
-        );
-      }
-      console.log(doseeq);
-      return Math.round(doseeq * 100) / 100;
+      console.log(doseEquivalente);
+      return Math.round(doseEquivalente * 100) / 100;
     } else {
       return null;
     }
   }
+
 }
 </script>
--- src/bean/Organ.ts
+++ src/bean/Organ.ts
@@ -1,23 +1,24 @@
 export default class Organ {
-    constructor(readonly name: string, readonly value: number) { }
+    constructor(readonly name: string, readonly value: number, readonly doseMax?: number) {
+    }
 }
 
 export const ALL_ORGANS = [
     new Organ("cartilage", 1),
-    new Organ("cœur", 3),
-    new Organ("foie", 1),
-    new Organ("grêle", 2),
-    new Organ("moelle", 2),
+    new Organ("cœur", 3, 35),
+    new Organ("foie", 1, 26),
+    new Organ("grêle", 2, 50),
+    new Organ("moelle", 2, 45),
     new Organ("ovaire", 1),
-    new Organ("œil", 1),
+    new Organ("œil", 1, 35),
     new Organ("os", 3),
-    new Organ("parotide", 2),
+    new Organ("parotide", 2, 30),
     new Organ("poumon", 3),
-    new Organ("rein", 3),
+    new Organ("rein", 3, 20),
     new Organ("rectum", 5),
-    new Organ("systême nerveux", 3),
+    new Organ("système nerveux", 3),
     new Organ("testicule", 1),
     new Organ("thyroïde", 3),
-    new Organ("tronc cérébral", 3),
+    new Organ("tronc cérébral", 3, 50),
     new Organ("vessie", 2),
 ]
